e3b4b6d23f2fbd67f6319fd0b86bbbc53f0bdc6d
Matthew Colbourne <mattcolbourne24@gmail.com>
2025-07-31 01:05:10 -0230

    added buncha collision for office

--- dialogue_ui.gd
+++ dialogue_ui.gd
@@ -1,38 +1,38 @@
 extends Control
 
 
-var dialogueDict = {
-	"bossFirstQ" : load("res://dialogue/BossQuestions1.dialogue"),
-	"playerFirstR" : load("res://dialogue/PlayerResponse1.dialogue"),
-	"randomEmpFirst" : load("res://dialogue/PlayerResponse1.dialogue")
-	
-}
+var meetingQuestions = load("res://dialogue/MeetingQuestions.dialogue")
 var trackedTurn : String = "boss"
+
 func _ready() -> void:
 	DialogueManager.connect("dialogue_ended", Callable(self, "_on_dialogue_ended"))
-	$AnimationPlayer.play("setBossTurn")
-	start_dialogue(dialogueDict.get("bossFirstQ"))
-	
+	$bossAnimPlayer.play("setBossTurnOn")
+	start_dialogue(meetingQuestions)
+
 func start_dialogue(dialogue) -> void:
 	DialogueManager.show_dialogue_balloon(dialogue)
 
 func _process(delta: float) -> void:
 	if GameManager.currentTurn != trackedTurn:
-		trackedTurn = GameManager.currentTurn
-		if trackedTurn == "player":
-			$AnimationPlayer.play("setPlayerTurn")
-		elif trackedTurn == "boss":
-			$AnimationPlayer.play("setBossTurn")
-		elif trackedTurn == "randomEmployee":
-			$AnimationPlayer.play("setRandomEmpTurn")
+		if GameManager.currentTurn != "randomEmployee":
+			$randomEmpAnimPlayer.play("setRandomEmpTurnOff")
+			
+		if GameManager.currentTurn == "player":
+			$bossAnimPlayer.play("setBossTurnOff")
+			$playerAnimPlayer.play("setPlayerTurnOn")
+			
+		elif GameManager.currentTurn == "boss" and trackedTurn != "randomEmployee":
+			$bossAnimPlayer.play("setBossTurnOn")
+			
+		elif GameManager.currentTurn == "randomEmployee":
+			$randomEmpAnimPlayer.play("setRandomEmpTurnOn")
 		
-	
+		
+		trackedTurn = GameManager.currentTurn
+
 func _on_dialogue_ended(resource) -> void:
-	
-	
 	if GameManager.endedConversation == false:
 		pass
-		
 	else:
 		#end game
 		print("endedmeeting")
