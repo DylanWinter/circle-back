e51fd82d7ed97079f98521d2717ce22d53e6d39d
Matthew Colbourne <mattcolbourne24@gmail.com>
2025-07-31 18:15:15 -0230

    Add NPC sprite flipping based on movement direction
    Introduced logic to flip the NPC sprite horizontally when movement direction changes, using a timer and position tracking. Updated the gossiper NPC scene to use the new sprite and timer setup. Minor cleanup in player.gd.

--- NPC.gd
+++ NPC.gd
@@ -4,8 +4,15 @@ var test = load("res://dialogue/test.dialogue")
 @export var coworker_title : String
 @export var walk_path : Line2D
 
-var walkSpeed = 30
+var walkSpeed : int = 30
+var lastPosition : Vector2
 
+func shouldFlipBasedOnMovement(lastPosition : Vector2, currentPosition : Vector2) -> bool:
+	if currentPosition.x - lastPosition.x < 0:
+		return true
+	else:
+		return false
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DialogueManager.connect("dialogue_ended", Callable(self, "_on_dialogue_ended"))
@@ -13,9 +20,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.z_index = position.y
+	lastPosition = $StaticBody2D.position
+	print(lastPosition)
 	$Path2D/PathFollow2D.progress += walkSpeed*delta
 	$StaticBody2D.position = $Path2D/PathFollow2D.position
+	print($StaticBody2D.position)
+	
+		
+	z_index = $StaticBody2D.global_position.y
+	
+		
+
 func start_dialogue() -> void:
 	GameManager.player.can_move = false
 	DialogueManager.show_dialogue_balloon(test)
@@ -32,3 +47,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if (body.is_in_group("Player") and GameManager.closest_interactable_npc == self):
 		GameManager.closest_interactable_npc = null
+
+
+func _on_flip_timer_timeout() -> void:
+	if shouldFlipBasedOnMovement(lastPosition, $StaticBody2D.position):
+		print("flip")
+		$StaticBody2D/npcSprite.scale.x *= -1.0
