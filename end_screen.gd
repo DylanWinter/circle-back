7b964b53234ec93b504e53659a935867c9ad7986
Dylan Winter <dlwinter@mun.ca>
2025-07-31 22:32:43 -0230

    basic win con

--- end_screen.gd
+++ end_screen.gd
@@ -1,5 +1,19 @@
 extends Control
 
+@onready var label = $Result
+
+func _ready() -> void:
+	var total = 0
+	var correct = 0
+	for term in GameManager.wordFoundDict:
+		total += 1
+		if GameManager.wordFoundDict[term]:
+			correct += 1
+	
+	if correct == total:
+		label.text = "You win!"
+	else:
+		label.text = "Fired!"
 
 func _on_restart_pressed() -> void:
 	SceneTransition.change_scene("res://office.tscn")
