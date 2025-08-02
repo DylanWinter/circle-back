15e28edf9bc1da81c0f3e8f47294d2749b879ff9
Matthew Colbourne <mattcolbourne24@gmail.com>
2025-08-02 03:24:32 -0230

    added a cubicle

---
+++ assets/Office assets/cubicle_node_i.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for node in get_children():
+		node.z_index = node.get_node("groundPos").global_position.y
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
