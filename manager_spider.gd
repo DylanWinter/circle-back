9544cfadf05bb93458ede6914c19aa06679635d0
Matthew Colbourne <mattcolbourne24@gmail.com>
2025-08-02 22:38:13 -0230

    added bounce up animation

---
+++ manager_spider.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+			
+func shouldFlipBasedOnMovement(lastPosition : Vector2, currentPosition : Vector2, sprite : Sprite2D) -> bool:
+	var direction : Vector2
+	direction = currentPosition - lastPosition
+	var shouldFlip = true
+	if currentPosition.x - lastPosition.x < 0 and sprite.scale.x>0:
+		shouldFlip = false
+	elif currentPosition.x - lastPosition.x > 0 and sprite.scale.x<0:
+		shouldFlip = false
+	
+	return shouldFlip
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	$npcSprite.z_index=$npcSprite/groundPos.global_position.y
+	var lastPosition = $npcSprite.position
+	
+	$Path2D/PathFollow2D.progress += 100*delta
+	$npcSprite.position = $Path2D/PathFollow2D.position
+	if shouldFlipBasedOnMovement(lastPosition, $npcSprite.position,$npcSprite):
+		$npcSprite.scale.x *= -1.0
--- office.gd
+++ office.gd
@@ -7,3 +7,8 @@ func _ready():
 func _process(delta: float) -> void:
 	#print(GameManager.wordFoundDict["RDC - Rival Demonic Company"])
 	pass
+	
+
+
+func _on_armless_only_block_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
