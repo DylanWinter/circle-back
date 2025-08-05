82a5e2f33a6140c0a9b835d2883e6028cd1a038c
Matthew Colbourne <mattcolbourne24@gmail.com>
2025-08-05 03:52:00 -0230

    ordered node structure

--- office.gd
+++ office.gd
@@ -4,6 +4,8 @@ func _ready():
 	GameManager.reset_loop()
 	Music.switch("chill")
 
+
+var playerNear = false
 func _process(delta: float) -> void:
 	#print(GameManager.wordFoundDict["RDC - Rival Demonic Company"])
 	pass
