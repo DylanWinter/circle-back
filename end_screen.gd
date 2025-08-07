3f9d49d9920cc7e45b1bcaa01db3196ea7e3d76c
Matthew Colbourne <mattcolbourne24@gmail.com>
2025-08-07 02:59:12 -0230

    fixed end screen text to fit

--- end_screen.gd
+++ end_screen.gd
@@ -12,20 +12,20 @@ func _ready() -> void:
 	
 	if GameManager.correctAnswers == total:
 		label.text = "TOTAL SUCCESS: GOOD END"
-		desc.text = "“Wow. You actually did it?” Did you like... look up a guide? \nWell anyways, enjoy rush hour.” 5/5"
+		desc.text = "“Wow. You actually did it?” Did you like... look up a guide? \nWell anyways, enjoy rush hour. 5 / 5"
 		sprite.texture = win_image
 	elif GameManager.correctAnswers == 0:
 		label.text = "TOTAL FAILURE: BAD END 1"
-		desc.text = "You died of embarrassment. Right there. \nOn the Floor. That was more embarrassing than that one time... \nYou remember that one time. Making more work for ther Janitor... Shame on you” 0/5"
+		desc.text = "You died of embarrassment. Right there. \nOn the Floor. That was more embarrassing than that one time. \nYou remember that one time. \nMaking more work for the Janitor... Shame on you. 0 / 5"
 		sprite.texture = superLossImage
 	else:
 		label.text = "FAILURE: BAD END 2"
-		desc.text = "You left the meeting room more red than your boss. \nMaybe you shouldn’t have stayed up all night painting your rock collection.” 1-4"
+		desc.text = "You left the meeting room more red than your boss. \nMaybe you shouldn’t have stayed up all night \npainting your rock collection. 1 / 5"
 		sprite.texture = loss_image
 
 func _on_restart_pressed() -> void:
 
-	var total = 4
+	var total = 5
 		
 	if GameManager.correctAnswers == total:
 		GameManager.full_reset()
