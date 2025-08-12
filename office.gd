64984fccd548aeb40adc0eb877c88a4b727f87f5
Matthew Colbourne <mattcolbourne24@gmail.com>
2025-08-11 22:04:43 -0230

    added start  of settings menu

--- office.gd
+++ office.gd
@@ -4,7 +4,6 @@ func _ready():
 	GameManager.reset_loop()
 	Music.switch("chill")
 
-
 var playerNear = false
 func _process(delta: float) -> void:
 	#print(GameManager.wordFoundDict["RDC - Rival Demonic Company"])
